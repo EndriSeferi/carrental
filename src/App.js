e6fb1888b0d57bd9733be39ae3c136d0855050ec
Endri Seferi <endriseferi98@gmail.com>
2021-12-01 14:38:51 +0100

    Test from Desktop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import MyNavbar from "./Components/Navbar";
 import Home from "./Pages/home";
 import MyCarousel from "./Components/MyCarousel";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
       <MyNavbar />
       <Home />
       <MyCarousel />
+      <Footer />
     </div>
   );
 }
--- src/Components/MyCarousel.js
+++ src/Components/MyCarousel.js
@@ -1,46 +1,35 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
 import "./MyCarousel.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import CarCard from "./CarCard";
 function MyCarousel() {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <div className="big-carousel">
       <h2>--------OUR BEST CARS</h2>
-      <Carousel>
-        <Carousel.Item interval={2000} className="carousel__item">
-          <img
-            className="carousel__image"
-            src="https://mpng.subpng.com/20190207/gbf/kisspng-mercedes-benz-a-class-car-mercedes-benz-cla-class-5c5c357592c695.7212954215495468696012.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-            className="carousel__image"
-            src="https://mpng.subpng.com/20190207/gbf/kisspng-mercedes-benz-a-class-car-mercedes-benz-cla-class-5c5c357592c695.7212954215495468696012.jpg"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="carousel__image"
-            src="https://mpng.subpng.com/20190207/gbf/kisspng-mercedes-benz-a-class-car-mercedes-benz-cla-class-5c5c357592c695.7212954215495468696012.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Carousel responsive={responsive} showDots={true} className="carousel">
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
       </Carousel>
     </div>
   );
